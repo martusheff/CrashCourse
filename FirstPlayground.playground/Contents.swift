17dd229d871bb0549f8516b7496a83170770efd0
Andronick Martusheff <71955336+martusheff@users.noreply.github.com>
2020-12-20 11:04:39 -0800

    organized

--- FirstPlayground.playground/Contents.swift
+++ FirstPlayground.playground/Contents.swift
@@ -1,3 +1,4 @@
+// Core Data Type/Declarations
 var playerName = "Alice"
 var age = 21
 var temperature = 72.6
@@ -5,44 +6,38 @@ var activeMembership = true
 
 // Type Inference - Swift infers the data type, and sets it to it.
 // Var is the only way to declare variables.
-
 age = age + 5
 
-var bonusScore: Int // Without providing value for inference, assign var a type. (Type annotation)
+// Without providing value for inference, assign var a type. (Type annotation)
+var bonusScore: Int
 var environmentName: String
 var levelCompleted: Bool
 var progressPercentage: Double
 
-// Core Swift data types.
-// Int, String, Double, Float, Bool, UInt, Character
-
+// Let = Constant
 let a = 5
 let b = 2
-
 let myResult = a / b
 
+// Type Conversion.
 var score = 1
 var highScore = 100.0
-
 highScore = Double(score)
 
 var message = "this is a message"
-
 var number = 3
-
 message = String(number)
 
 let myString = "kdasl"
-
 let myResultV2 = Int(myString)
 
+
+// Optionals "?"
 var firstName: String?
 var lastName: String?
 var userAge: Int?
 
-
 // Using Optionals
-
 var regularInt: Int
 var optionalInt: Int?
 
@@ -68,20 +63,19 @@ if let unwrappedInt = optionalInt {
 // Swift Arrays
 var musicalModes = ["Ionian", "Dorian", "Phrygian", "Lydian"]
 let speedLimits = [15, 25, 30, 35, 40, 50, 60, 75, 100]
-
 let initialModes = musicalModes[0]
 
 //change value
 musicalModes[3] = "Aeolian"
 musicalModes.append("Locrian")
 // add it again
-
 musicalModes.append("Locrian")
 
+// Remove element, and set it to constant theRemovedElement
 let theRemovedElement = musicalModes.removeLast()
 
-var myStringAwway: [String]
-var myIntArray: [Int] = []
+var myStringArray: [String] // Tell Swift what kind of array you are making.
+var myIntArray: [Int] = [] // Create empty array.
 
 // If statements
 var myScore = 100
@@ -94,9 +88,7 @@ if myScore > 100 {
     print("Sorry, maybe next time.")
 }
 
-
 // Switch statements
-
 var abbrev = "MB"
 
 switch abbrev {
@@ -109,8 +101,8 @@ default:
 }
 
 
+// More switch statements
 let volcanoExplosivityIndex: Int
-
 volcanoExplosivityIndex = 2
 
 switch volcanoExplosivityIndex {
@@ -124,6 +116,68 @@ case 3:
     print("Catastrohic ☹️")
 case 4...11:
     print("Anywhere from 4 to 11, inclusive.")
+
 default:
     print("Not a recognized index.")
 }
+
+// Strides
+for number in stride(from: 0, to: 256, by: 5) {
+    print(number)
+}
+
+// String Interpolation
+var trackname = "Ambre"
+var artistName = "Nils Frahm"
+var duration = 228
+
+// ...
+let messageV2 = "Now playing \(trackname) by \(artistName) which is \(duration) long."
+print(messageV2)
+print("Now playing \(trackname) by \(artistName) which is \(duration / 60)m \(duration % 60)s long.")
+
+
+// Creating Functions
+func showMessage(number: Int, name: String) {
+    var newNumber = number
+    newNumber += 5
+    print("Hello \(name), the function call worked, and we passed in int of \(number)")
+}
+showMessage(number: 5, name: "Andron")
+
+// Returning values
+func basicFunction() -> String {
+    let str = "This is a simple function."
+    return str
+}
+let result = basicFunction()
+print(result)
+
+//Ignoring returned Values
+func calculateValue() -> Int {
+    print("Function successfully called.")
+    return 99
+}
+
+func applicationLaunched(_ aNotification: Int) {
+    _ = calculateValue()
+}
+
+// Omitting Argument Labels
+func show(message: String) {
+    print("The text passed in was: \(message)")
+}
+// call
+show(message: "This seems a little redundant.")
+
+
+//  Changing Argument Labels
+func difference(between firstAmount: Int, and secondAmount: Int) -> Int {
+    if firstAmount > secondAmount {
+        return firstAmount - secondAmount
+    } else {
+        return secondAmount - firstAmount
+    }
+}
+let resultV2 = difference(between: 27391, and: 7863)
+print("The difference is: \(resultV2).")
