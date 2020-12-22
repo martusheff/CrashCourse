cd9ba0bebcf19a37ce4b8174ab03074f9e9dedf3
Andronick Martusheff <71955336+martusheff@users.noreply.github.com>
2020-12-21 23:07:50 -0800

    List Views within App
    Learning how to implement different types of views.

--- iOS 14 Getting Started/First iOS App/First iOS App/ContentView.swift
+++ iOS 14 Getting Started/First iOS App/First iOS App/ContentView.swift
@@ -6,26 +6,12 @@
 //
 
 import SwiftUI
+var str = ""
+
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Circle()
-            HStack{
-                Spacer()
-                Rectangle()
-                Spacer()
-            }
-            HStack {
-                Spacer(minLength: 50)
-                RoundedRectangle(cornerRadius: 25.0)
-                Spacer(minLength: 100)
-                RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
-                Spacer(minLength: 50)
-            }
-        }
-        Text("Hello, World!")
-            .padding()
+        EmptyView()
     }
 }
 
--- iOS 14 Getting Started/NightWatch/NightWatch/ContentView.swift
+++ iOS 14 Getting Started/NightWatch/NightWatch/ContentView.swift
@@ -9,11 +9,92 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Nightly Tasks")
-            Text("Weekly Tasks")
-            Text("Monthly Tasks")
+        HStack {
+            VStack(alignment: .leading) {
+                
+                
+                // MARK: NIGHTLY TASKS
+                Group {
+                    HStack {
+                        Text(Image(systemName: "moon.stars"))
+                            .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
+                            .fontWeight(.heavy)
+                            .foregroundColor(/*@START_MENU_TOKEN@*/.yellow/*@END_MENU_TOKEN@*/)
+                            .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                        Text("Nightly Tasks")
+                            
+                            .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
+                            .fontWeight(.heavy)
+                            .foregroundColor(/*@START_MENU_TOKEN@*/.yellow/*@END_MENU_TOKEN@*/)
+                            .underline()
+                            .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+
+                    }
+                    .padding(.bottom, 1.0)
+                        
+                    Text("- Check all windows.")
+                    Text("- Check all doors.")
+                    Text("- Check that the safe is locked.")
+                    Text("- Check the mailbox.")
+                    Text("- Inspect security cameras.")
+                }
+                
+                // MARK: WEEKLY TASKS
+                Group {
+                    Divider()
+                        .padding(.bottom, 1.0)
+                    HStack {
+                        Text(Image(systemName: "sunset"))
+                            .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
+                            .fontWeight(.heavy)
+                            .foregroundColor(/*@START_MENU_TOKEN@*/.yellow/*@END_MENU_TOKEN@*/)
+                            .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                        
+                        Text("Weekly Tasks")
+                            .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
+                            .fontWeight(.heavy)
+                            .foregroundColor(/*@START_MENU_TOKEN@*/.yellow/*@END_MENU_TOKEN@*/)
+                            .underline()
+                            .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+                        
+                            
+                    }
+                    .padding(.bottom, 1.0)
+                    Text("- Check inside all vacant rooms.")
+                    Text("- Walk the perimeter of property.")
+                }
+                
+                // MARK: MONTHLY TASKS
+                Group {
+                    Divider()
+                        .padding(.bottom, 1.0)
+                    HStack {
+                        Text(Image(systemName: "calendar"))
+                            .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
+                            .fontWeight(.heavy)
+                            .foregroundColor(/*@START_MENU_TOKEN@*/.yellow/*@END_MENU_TOKEN@*/)
+                            .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                        
+                        Text("Monthly Tasks")
+                            .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
+                            .fontWeight(.heavy)
+                            .foregroundColor(/*@START_MENU_TOKEN@*/.yellow/*@END_MENU_TOKEN@*/)
+                            .underline()
+                            .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+                    }
+                    .padding(.bottom, 1.0)
+                    Text("- Test security alarm.")
+                    Text("- Test motion detectors.")
+                    Text("- Test smoke alarms.")
+                }
+                
+                Spacer()
+                    
+            }
+            .foregroundColor(/*@START_MENU_TOKEN@*/.gray/*@END_MENU_TOKEN@*/)
+            Spacer()
         }
+        .padding(.all)
     }
 }
 
