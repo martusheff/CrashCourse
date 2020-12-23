7eb940e0428d60b346cfa0a6dd98ee12a45dafbe
Andronick Martusheff <71955336+martusheff@users.noreply.github.com>
2020-12-23 00:20:24 -0800

    data model

---
+++ iOS 14 Getting Started/NightWatch/NightWatch/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  NightWatch
+//
+//  Created by andronick martusheff on 12/23/20.
+//
+
+import Foundation
+
+struct Task {
+    let name: String
+    var isComplete: Bool
+    var lastCompleted: Date?
+}
