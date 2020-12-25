3a4daf52c8775b62d7d7b28ace6721e61d15eb16
Andronick Martusheff <71955336+martusheff@users.noreply.github.com>
2020-12-25 14:00:49 -0800

    fin

---
+++ iOS 14 Getting Started/Swift Fundamentals/Swift Fundamentals/Swift_FundamentalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_FundamentalsApp.swift
+//  Swift Fundamentals
+//
+//  Created by andronick martusheff on 12/25/20.
+//
+
+import SwiftUI
+
+@main
+struct Swift_FundamentalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
